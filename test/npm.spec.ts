a698482be10edf3f2515b2bee56c0305a9216721
Maji <steeven.regnault@outlook.fr>
2022-05-20 14:14:00 +0200

    ✅ test: create tests for the build command

--- test/npm.spec.ts
+++ test/npm.spec.ts
@@ -1,10 +1,11 @@
 import assert from 'assert';
-import { getPackageLatestVersionUrl } from './npm';
+import { getPackageLatestVersionUrl } from '../src/tools/npm';
+import { tenSec } from './tools';
 
 describe('Archive suite', () => {
   it('should be able to resolve npm latest package version', async () => {
     assert.ok(await getPackageLatestVersionUrl('@candy-doc/board'));
-  });
+  }).timeout(tenSec);
   it('should throw if not able to resolve npm package', async () => {
     assert.rejects(async () => await getPackageLatestVersionUrl('@candy-doc/boardrr'));
   });
